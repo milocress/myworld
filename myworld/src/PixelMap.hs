5e351cd93929d3a2dff514dbe784c0e0750f2446
Milo Cress <milocress@cvsdvt.org>
2018-08-04 23:41:57 -0400

    removed obsolete documentation (if one could call it that)

--- myworld/src/PixelMap.hs
+++ myworld/src/PixelMap.hs
@@ -11,4 +11,3 @@ type PixelMap = Map PixelRGB8
 
 fromPixelMap :: PixelMap -> Resolution -> Image PixelRGB8
 fromPixelMap m (Resolution w h) = generateImage (\x y -> runMap m $ Planar (fromIntegral x) (fromIntegral y)) w h
--- f is a constructor for PlanarCoordinate.
